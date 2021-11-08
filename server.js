23b18a1146be8daff9a41e893c5025bef2329894
Ana A <aausek@users.noreply.github.com>
2021-11-07 23:28:39 -0500

    Removing DB creds

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const fs = require('fs');
-const cors = require('cors')
-const oracledb = require('oracledb');
-const dbConfig = require('./dbConfig.js');
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const oracledb = require("oracledb");
+const dbConfig = require("./dbConfig.js");
 const app = express();
 
 const PORT = 3001;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 let libPath;
-if (process.platform === 'win32') {           // Windows
-  libPath = 'C:\\oracle\\instantclient_19_12';
-} else if (process.platform === 'darwin') {   // macOS
-  libPath = process.env.HOME + '/Downloads/instantclient_19_8';
+if (process.platform === "win32") {
+  // Windows
+  libPath = "C:\\oracle\\instantclient_19_12";
+} else if (process.platform === "darwin") {
+  // macOS
+  libPath = process.env.HOME + "/Downloads/instantclient_19_8";
 }
 if (libPath && fs.existsSync(libPath)) {
   oracledb.initOracleClient({ libDir: libPath });
@@ -25,16 +27,13 @@ async function run() {
   let connection;
 
   try {
-
     let sql, binds, options, result;
 
-    connection = await oracledb.getConnection(
-      {
-        user          : "aausek",
-        password      : "Ausek6493",
-        connectString : "//oracle.cise.ufl.edu/orcl"
-      }
-    );
+    connection = await oracledb.getConnection({
+      user: "",
+      password: "",
+      connectString: "//oracle.cise.ufl.edu/orcl",
+    });
 
     //
     // Create a table
@@ -43,15 +42,14 @@ async function run() {
     const stmts = [
       `DROP TABLE no_example`,
 
-      `CREATE TABLE no_example (id NUMBER, data VARCHAR2(20))`
+      `CREATE TABLE no_example (id NUMBER, data VARCHAR2(20))`,
     ];
 
     for (const s of stmts) {
       try {
         await connection.execute(s);
       } catch (e) {
-        if (e.errorNum != 942)
-          console.error(e);
+        if (e.errorNum != 942) console.error(e);
       }
     }
 
@@ -62,9 +60,9 @@ async function run() {
     sql = `INSERT INTO no_example VALUES (:1, :2)`;
 
     binds = [
-      [101, "Alpha" ],
-      [102, "Beta" ],
-      [103, "Gamma" ]
+      [101, "Alpha"],
+      [102, "Beta"],
+      [103, "Gamma"],
     ];
 
     // For a complete list of options see the documentation.
@@ -73,27 +71,25 @@ async function run() {
       // batchErrors: true,  // continue processing even if there are data errors
       bindDefs: [
         { type: oracledb.NUMBER },
-        { type: oracledb.STRING, maxSize: 20 }
-      ]
+        { type: oracledb.STRING, maxSize: 20 },
+      ],
     };
 
     result = await connection.executeMany(sql, binds, options);
 
     console.log("Number of rows inserted:", result.rowsAffected);
-    
+
     //
     // Query the data
     //
-    
+
     sql = `SELECT * FROM no_example`;
-    
+
     binds = {};
-    
-    
 
     // For a complete list of options see the documentation.
     options = {
-      outFormat: oracledb.OUT_FORMAT_OBJECT,   // query result format
+      outFormat: oracledb.OUT_FORMAT_OBJECT, // query result format
       // extendedMetaData: true,               // get extra metadata
       // prefetchRows:     100,                // internal buffer allocation size for tuning
       // fetchArraySize:   100                 // internal buffer allocation size for tuning
@@ -109,20 +105,19 @@ async function run() {
     //
     // Show the date.  The value of ORA_SDTZ affects the output
     //
-    
+
     //sql = `SELECT TO_CHAR(CURRENT_DATE, 'DD-Mon-YYYY HH24:MI') AS CD FROM DUAL`;
     result = await connection.execute(sql, binds, options);
     console.log("Current date query results: ");
-    console.log(result.rows[0]['CD']);
-    
-    app.get('/api', (req, res) => {
-      res.send(result.rows)
+    console.log(result.rows[0]["CD"]);
+
+    app.get("/api", (req, res) => {
+      res.send(result.rows);
     });
 
-    app.get('/home', (req, res) => {
-      res.send({ express: 'Hello From Express' });
+    app.get("/home", (req, res) => {
+      res.send({ express: "Hello From Express" });
     });
-
   } catch (err) {
     console.error(err);
   } finally {
@@ -134,18 +129,17 @@ async function run() {
       }
     }
   }
-  
 }
 
 run();
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-    
+  app.use(express.static(path.join(__dirname, "client/build")));
+
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
@@ -153,7 +147,7 @@ app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
 
-process.on('SIGINT', function() {
-  console.log( "\nStopping server (Ctrl-C)" );
+process.on("SIGINT", function () {
+  console.log("\nStopping server (Ctrl-C)");
   process.exit(0);
 });
